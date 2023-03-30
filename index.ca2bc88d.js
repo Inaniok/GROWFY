c80e363f0d388e8611b51c0005fd3ba06fea48f6
Inaniok <106915226+Inaniok@users.noreply.github.com>
2023-03-30 16:58:47 +0000

    Deploying to gh-pages from @ Inaniok/GROWFY@0ea2fd7a48111a972093e4c3243b1a1e370636aa 🚀

---
+++ index.ca2bc88d.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",(function(e){e.target;e.target.closest(".icon-menu")&&document.documentElement.classList.toggle("menu-open")}));
+//# sourceMappingURL=index.ca2bc88d.js.map
---
+++ index.f7f9ba91.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",(function(e){e.target,e.target.closest(".icon-menu")&&document.documentElement.classList.toggle("menu-open")}));
+//# sourceMappingURL=index.f7f9ba91.js.map
